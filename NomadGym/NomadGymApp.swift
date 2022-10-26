c76236e969f42dcedc72346f7bdb6c7eb58d0e3d
damondeletter <damon.deletter@student.hogent.be>
2022-10-26 10:40:03 +0200

    Initial Commit

---
+++ NomadGym/NomadGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomadGymApp.swift
+//  NomadGym
+//
+//  Created by Damon De Letter on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NomadGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
